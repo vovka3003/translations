acbcbb09b5bdbd994b70f14fd6bf78bccea7eb5c
Ben Keen <ben.keen@gmail.com>
2015-05-03 20:49:23 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,11 @@
+## Translations
+
+This repository contains all translation files for the Form Tools core. 
+
+### Help translate! 
+
+We LOVE to get help with the translations! 
+
+### Languages
+
+- [Afrikaans](core/af.php)
---
+++ core/summary.php
@@ -0,0 +1,716 @@
+<?php
+
+$OT_info = array();
+$OT_info["export_time"] = 1384743626;
+
+// version information
+$OT_version = array();
+$OT_version["version_id"] = 23;
+$OT_version["version_label"] = "2.1.x and later";
+$OT_version["version_last_modified"] = "2011-08-27 18:19:09";
+$OT_version["version_last_modified"] = "2011-08-27 18:19:09";
+
+// translation information
+$OT_translations = array (
+  3 => 
+  array (
+    'language_name' => 'Afrikaans',
+    'php_export_status' => 'Complete',
+    'php_filename' => 'af.php',
+    'percent_reliability' => '0',
+    'percent_translated' => '100',
+    'percent_unreviewed_auto_translations' => 100,
+    'num_unreviewed_auto_translations' => '1189',
+    'translators' => 
+    array (
+    ),
+  ),
+  5 => 
+  array (
+    'language_name' => 'Arabic',
+    'php_export_status' => 'Complete',
+    'php_filename' => 'ar.php',
+    'percent_reliability' => '0',
+    'percent_translated' => '100',
+    'percent_unreviewed_auto_translations' => 94,
+    'num_unreviewed_auto_translations' => '1117',
+    'translators' => 
+    array (
+    ),
+  ),
+  8 => 
+  array (
+    'language_name' => 'Azerbaijani',
+    'php_export_status' => 'Complete',
+    'php_filename' => 'az.php',
+    'percent_reliability' => '0',
+    'percent_translated' => '3',
+    'percent_unreviewed_auto_translations' => 0,
+    'num_unreviewed_auto_translations' => '0',
+    'translators' => 
+    array (
+    ),
+  ),
+  11 => 
+  array (
+    'language_name' => 'Bulgarian',
+    'php_export_status' => 'Complete',
+    'php_filename' => 'bg.php',
+    'percent_reliability' => '0',
+    'percent_translated' => '100',
+    'percent_unreviewed_auto_translations' => 100,
+    'num_unreviewed_auto_translations' => '1189',
+    'translators' => 
+    array (
+    ),
+  ),
+  10 => 
+  array (
+    'language_name' => 'Byelorussian',
+    'php_export_status' => 'Complete',
+    'php_filename' => 'be.php',
+    'percent_reliability' => '0',
+    'percent_translated' => '100',
+    'percent_unreviewed_auto_translations' => 100,
+    'num_unreviewed_auto_translations' => '1189',
+    'translators' => 
+    array (
+    ),
+  ),
+  17 => 
+  array (
+    'language_name' => 'Catalan',
+    'php_export_status' => 'Complete',
+    'php_filename' => 'ca.php',
+    'percent_reliability' => '0',
+    'percent_translated' => '100',
+    'percent_unreviewed_auto_translations' => 100,
+    'num_unreviewed_auto_translations' => '1189',
+    'translators' => 
+    array (
+    ),
+  ),
+  43 => 
+  array (
+    'language_name' => 'Croatian',
+    'php_export_status' => 'Complete',
+    'php_filename' => 'hr.php',
+    'percent_reliability' => '2',
+    'percent_translated' => '100',
+    'percent_unreviewed_auto_translations' => 91,
+    'num_unreviewed_auto_translations' => '1081',
+    'translators' => 
+    array (
+    ),
+  ),
+  19 => 
+  array (
+    'language_name' => 'Czech',
+    'php_export_status' => 'Complete',
+    'php_filename' => 'cs.php',
+    'percent_reliability' => '0',
+    'percent_translated' => '100',
+    'percent_unreviewed_auto_translations' => 99,
+    'num_unreviewed_auto_translations' => '1176',
+    'translators' => 
+    array (
+    ),
+  ),
+  21 => 
+  array (
+    'language_name' => 'Danish',
+    'php_export_status' => 'Complete',
+    'php_filename' => 'da.php',
+    'percent_reliability' => '0',
+    'percent_translated' => '100',
+    'percent_unreviewed_auto_translations' => 78,
+    'num_unreviewed_auto_translations' => '927',
+    'translators' => 
+    array (
+    ),
+  ),
+  82 => 
+  array (
+    'language_name' => 'Dutch',
+    'php_export_status' => 'Complete',
+    'php_filename' => 'nl.php',
+    'percent_reliability' => '10',
+    'percent_translated' => '100',
+    'percent_unreviewed_auto_translations' => 49,
+    'num_unreviewed_auto_translations' => '584',
+    'translators' => 
+    array (
+      0 => 'Sam Zenner',
+    ),
+  ),
+  25 => 
+  array (
+    'language_name' => 'English',
+    'php_export_status' => 'Complete',
+    'php_filename' => 'en_us.php',
+    'percent_reliability' => '0',
+    'percent_translated' => '0',
+    'percent_unreviewed_auto_translations' => 0,
+    'num_unreviewed_auto_translations' => '0',
+    'translators' => 
+    array (
+    ),
+  ),
+  28 => 
+  array (
+    'language_name' => 'Estonian',
+    'php_export_status' => 'Complete',
+    'php_filename' => 'et.php',
+    'percent_reliability' => '0',
+    'percent_translated' => '100',
+    'percent_unreviewed_auto_translations' => 100,
+    'num_unreviewed_auto_translations' => '1189',
+    'translators' => 
+    array (
+    ),
+  ),
+  120 => 
+  array (
+    'language_name' => 'Filipino',
+    'php_export_status' => 'Complete',
+    'php_filename' => 'tl.php',
+    'percent_reliability' => '0',
+    'percent_translated' => '100',
+    'percent_unreviewed_auto_translations' => 100,
+    'num_unreviewed_auto_translations' => '1189',
+    'translators' => 
+    array (
+    ),
+  ),
+  31 => 
+  array (
+    'language_name' => 'Finnish',
+    'php_export_status' => 'Complete',
+    'php_filename' => 'fi.php',
+    'percent_reliability' => '0',
+    'percent_translated' => '100',
+    'percent_unreviewed_auto_translations' => 88,
+    'num_unreviewed_auto_translations' => '1043',
+    'translators' => 
+    array (
+    ),
+  ),
+  34 => 
+  array (
+    'language_name' => 'French',
+    'php_export_status' => 'Complete',
+    'php_filename' => 'fr.php',
+    'percent_reliability' => '2',
+    'percent_translated' => '100',
+    'percent_unreviewed_auto_translations' => 77,
+    'num_unreviewed_auto_translations' => '910',
+    'translators' => 
+    array (
+    ),
+  ),
+  38 => 
+  array (
+    'language_name' => 'Galician',
+    'php_export_status' => 'Complete',
+    'php_filename' => 'gl.php',
+    'percent_reliability' => '0',
+    'percent_translated' => '100',
+    'percent_unreviewed_auto_translations' => 100,
+    'num_unreviewed_auto_translations' => '1189',
+    'translators' => 
+    array (
+    ),
+  ),
+  22 => 
+  array (
+    'language_name' => 'German',
+    'php_export_status' => 'Complete',
+    'php_filename' => 'de.php',
+    'percent_reliability' => '2',
+    'percent_translated' => '100',
+    'percent_unreviewed_auto_translations' => 71,
+    'num_unreviewed_auto_translations' => '840',
+    'translators' => 
+    array (
+      0 => 'Krunoslav Dukic',
+    ),
+  ),
+  24 => 
+  array (
+    'language_name' => 'Greek',
+    'php_export_status' => 'Complete',
+    'php_filename' => 'el.php',
+    'percent_reliability' => '0',
+    'percent_translated' => '100',
+    'percent_unreviewed_auto_translations' => 100,
+    'num_unreviewed_auto_translations' => '1189',
+    'translators' => 
+    array (
+    ),
+  ),
+  42 => 
+  array (
+    'language_name' => 'Hindi',
+    'php_export_status' => 'Complete',
+    'php_filename' => 'hi.php',
+    'percent_reliability' => '0',
+    'percent_translated' => '100',
+    'percent_unreviewed_auto_translations' => 100,
+    'num_unreviewed_auto_translations' => '1189',
+    'translators' => 
+    array (
+    ),
+  ),
+  44 => 
+  array (
+    'language_name' => 'Hungarian',
+    'php_export_status' => 'Complete',
+    'php_filename' => 'hu.php',
+    'percent_reliability' => '0',
+    'percent_translated' => '100',
+    'percent_unreviewed_auto_translations' => 96,
+    'num_unreviewed_auto_translations' => '1139',
+    'translators' => 
+    array (
+    ),
+  ),
+  50 => 
+  array (
+    'language_name' => 'Icelandic',
+    'php_export_status' => 'Complete',
+    'php_filename' => 'is.php',
+    'percent_reliability' => '0',
+    'percent_translated' => '100',
+    'percent_unreviewed_auto_translations' => 100,
+    'num_unreviewed_auto_translations' => '1189',
+    'translators' => 
+    array (
+    ),
+  ),
+  49 => 
+  array (
+    'language_name' => 'Indonesian',
+    'php_export_status' => 'Complete',
+    'php_filename' => 'id.php',
+    'percent_reliability' => '0',
+    'percent_translated' => '100',
+    'percent_unreviewed_auto_translations' => 86,
+    'num_unreviewed_auto_translations' => '1021',
+    'translators' => 
+    array (
+    ),
+  ),
+  36 => 
+  array (
+    'language_name' => 'Irish',
+    'php_export_status' => 'Complete',
+    'php_filename' => 'ga.php',
+    'percent_reliability' => '0',
+    'percent_translated' => '100',
+    'percent_unreviewed_auto_translations' => 100,
+    'num_unreviewed_auto_translations' => '1189',
+    'translators' => 
+    array (
+    ),
+  ),
+  51 => 
+  array (
+    'language_name' => 'Italian',
+    'php_export_status' => 'Complete',
+    'php_filename' => 'it.php',
+    'percent_reliability' => '0',
+    'percent_translated' => '100',
+    'percent_unreviewed_auto_translations' => 46,
+    'num_unreviewed_auto_translations' => '548',
+    'translators' => 
+    array (
+      0 => 'Diego Meozzi',
+    ),
+  ),
+  53 => 
+  array (
+    'language_name' => 'Japanese',
+    'php_export_status' => 'Complete',
+    'php_filename' => 'ja.php',
+    'percent_reliability' => '0',
+    'percent_translated' => '100',
+    'percent_unreviewed_auto_translations' => 100,
+    'num_unreviewed_auto_translations' => '1189',
+    'translators' => 
+    array (
+    ),
+  ),
+  61 => 
+  array (
+    'language_name' => 'Korean',
+    'php_export_status' => 'Complete',
+    'php_filename' => 'ko.php',
+    'percent_reliability' => '0',
+    'percent_translated' => '100',
+    'percent_unreviewed_auto_translations' => 100,
+    'num_unreviewed_auto_translations' => '1189',
+    'translators' => 
+    array (
+    ),
+  ),
+  69 => 
+  array (
+    'language_name' => 'Latvian',
+    'php_export_status' => 'Complete',
+    'php_filename' => 'la.php',
+    'percent_reliability' => '0',
+    'percent_translated' => '100',
+    'percent_unreviewed_auto_translations' => 100,
+    'num_unreviewed_auto_translations' => '1189',
+    'translators' => 
+    array (
+    ),
+  ),
+  68 => 
+  array (
+    'language_name' => 'Lithuanian',
+    'php_export_status' => 'Complete',
+    'php_filename' => 'lt.php',
+    'percent_reliability' => '0',
+    'percent_translated' => '100',
+    'percent_unreviewed_auto_translations' => 100,
+    'num_unreviewed_auto_translations' => '1189',
+    'translators' => 
+    array (
+    ),
+  ),
+  72 => 
+  array (
+    'language_name' => 'Macedonian',
+    'php_export_status' => 'Complete',
+    'php_filename' => 'mk.php',
+    'percent_reliability' => '0',
+    'percent_translated' => '100',
+    'percent_unreviewed_auto_translations' => 100,
+    'num_unreviewed_auto_translations' => '1189',
+    'translators' => 
+    array (
+    ),
+  ),
+  77 => 
+  array (
+    'language_name' => 'Malay',
+    'php_export_status' => 'Complete',
+    'php_filename' => 'ms.php',
+    'percent_reliability' => '0',
+    'percent_translated' => '100',
+    'percent_unreviewed_auto_translations' => 100,
+    'num_unreviewed_auto_translations' => '1189',
+    'translators' => 
+    array (
+    ),
+  ),
+  78 => 
+  array (
+    'language_name' => 'Maltese',
+    'php_export_status' => 'Complete',
+    'php_filename' => 'mt.php',
+    'percent_reliability' => '0',
+    'percent_translated' => '100',
+    'percent_unreviewed_auto_translations' => 100,
+    'num_unreviewed_auto_translations' => '1189',
+    'translators' => 
+    array (
+    ),
+  ),
+  83 => 
+  array (
+    'language_name' => 'Norwegian',
+    'php_export_status' => 'Complete',
+    'php_filename' => 'no.php',
+    'percent_reliability' => '0',
+    'percent_translated' => '100',
+    'percent_unreviewed_auto_translations' => 76,
+    'num_unreviewed_auto_translations' => '908',
+    'translators' => 
+    array (
+      0 => 'Ole-Kristian de Voogd',
+    ),
+  ),
+  30 => 
+  array (
+    'language_name' => 'Persian',
+    'php_export_status' => 'Complete',
+    'php_filename' => 'fa.php',
+    'percent_reliability' => '0',
+    'percent_translated' => '100',
+    'percent_unreviewed_auto_translations' => 96,
+    'num_unreviewed_auto_translations' => '1136',
+    'translators' => 
+    array (
+      0 => 'mahdiar amani',
+    ),
+  ),
+  88 => 
+  array (
+    'language_name' => 'Polish',
+    'php_export_status' => 'Complete',
+    'php_filename' => 'pl.php',
+    'percent_reliability' => '1',
+    'percent_translated' => '100',
+    'percent_unreviewed_auto_translations' => 82,
+    'num_unreviewed_auto_translations' => '977',
+    'translators' => 
+    array (
+    ),
+  ),
+  90 => 
+  array (
+    'language_name' => 'Portuguese',
+    'php_export_status' => 'Complete',
+    'php_filename' => 'pt.php',
+    'percent_reliability' => '32',
+    'percent_translated' => '100',
+    'percent_unreviewed_auto_translations' => 84,
+    'num_unreviewed_auto_translations' => '998',
+    'translators' => 
+    array (
+    ),
+  ),
+  138 => 
+  array (
+    'language_name' => 'Portuguese (Brazilian)',
+    'php_export_status' => 'Complete',
+    'php_filename' => 'pt_br.php',
+    'percent_reliability' => '10',
+    'percent_translated' => '60',
+    'percent_unreviewed_auto_translations' => 0,
+    'num_unreviewed_auto_translations' => '0',
+    'translators' => 
+    array (
+      0 => 'Flávio Veras',
+    ),
+  ),
+  139 => 
+  array (
+    'language_name' => 'Portuguese (European)',
+    'php_export_status' => 'Complete',
+    'php_filename' => 'pt_eu.php',
+    'percent_reliability' => '0',
+    'percent_translated' => '23',
+    'percent_unreviewed_auto_translations' => 0,
+    'num_unreviewed_auto_translations' => '0',
+    'translators' => 
+    array (
+    ),
+  ),
+  94 => 
+  array (
+    'language_name' => 'Romanian',
+    'php_export_status' => 'Complete',
+    'php_filename' => 'ro.php',
+    'percent_reliability' => '0',
+    'percent_translated' => '100',
+    'percent_unreviewed_auto_translations' => 96,
+    'num_unreviewed_auto_translations' => '1143',
+    'translators' => 
+    array (
+    ),
+  ),
+  95 => 
+  array (
+    'language_name' => 'Russian',
+    'php_export_status' => 'Complete',
+    'php_filename' => 'ru.php',
+    'percent_reliability' => '0',
+    'percent_translated' => '100',
+    'percent_unreviewed_auto_translations' => 99,
+    'num_unreviewed_auto_translations' => '1180',
+    'translators' => 
+    array (
+    ),
+  ),
+  108 => 
+  array (
+    'language_name' => 'Serbian',
+    'php_export_status' => 'Complete',
+    'php_filename' => 'sr.php',
+    'percent_reliability' => '0',
+    'percent_translated' => '100',
+    'percent_unreviewed_auto_translations' => 100,
+    'num_unreviewed_auto_translations' => '1189',
+    'translators' => 
+    array (
+    ),
+  ),
+  135 => 
+  array (
+    'language_name' => 'Simplified Chinese',
+    'php_export_status' => 'Complete',
+    'php_filename' => 'zh_cn.php',
+    'percent_reliability' => '6',
+    'percent_translated' => '100',
+    'percent_unreviewed_auto_translations' => 77,
+    'num_unreviewed_auto_translations' => '919',
+    'translators' => 
+    array (
+    ),
+  ),
+  102 => 
+  array (
+    'language_name' => 'Slovak',
+    'php_export_status' => 'Complete',
+    'php_filename' => 'sk.php',
+    'percent_reliability' => '0',
+    'percent_translated' => '100',
+    'percent_unreviewed_auto_translations' => 89,
+    'num_unreviewed_auto_translations' => '1064',
+    'translators' => 
+    array (
+    ),
+  ),
+  103 => 
+  array (
+    'language_name' => 'Slovenian',
+    'php_export_status' => 'Complete',
+    'php_filename' => 'sl.php',
+    'percent_reliability' => '0',
+    'percent_translated' => '100',
+    'percent_unreviewed_auto_translations' => 60,
+    'num_unreviewed_auto_translations' => '712',
+    'translators' => 
+    array (
+    ),
+  ),
+  27 => 
+  array (
+    'language_name' => 'Spanish',
+    'php_export_status' => 'Complete',
+    'php_filename' => 'es.php',
+    'percent_reliability' => '6',
+    'percent_translated' => '100',
+    'percent_unreviewed_auto_translations' => 64,
+    'num_unreviewed_auto_translations' => '766',
+    'translators' => 
+    array (
+      0 => 'Luis Sendoya',
+      1 => 'Julio Fernandez',
+    ),
+  ),
+  113 => 
+  array (
+    'language_name' => 'Swahili',
+    'php_export_status' => 'Complete',
+    'php_filename' => 'sw.php',
+    'percent_reliability' => '0',
+    'percent_translated' => '70',
+    'percent_unreviewed_auto_translations' => 70,
+    'num_unreviewed_auto_translations' => '834',
+    'translators' => 
+    array (
+    ),
+  ),
+  112 => 
+  array (
+    'language_name' => 'Swedish',
+    'php_export_status' => 'Complete',
+    'php_filename' => 'sv.php',
+    'percent_reliability' => '0',
+    'percent_translated' => '100',
+    'percent_unreviewed_auto_translations' => 3,
+    'num_unreviewed_auto_translations' => '33',
+    'translators' => 
+    array (
+      0 => 'Peter Häggstrand',
+    ),
+  ),
+  117 => 
+  array (
+    'language_name' => 'Thai',
+    'php_export_status' => 'Complete',
+    'php_filename' => 'th.php',
+    'percent_reliability' => '0',
+    'percent_translated' => '100',
+    'percent_unreviewed_auto_translations' => 82,
+    'num_unreviewed_auto_translations' => '973',
+    'translators' => 
+    array (
+      0 => 'Krisada Pramualsap',
+    ),
+  ),
+  137 => 
+  array (
+    'language_name' => 'Traditional Chinese',
+    'php_export_status' => 'Complete',
+    'php_filename' => 'zh_tw.php',
+    'percent_reliability' => '0',
+    'percent_translated' => '100',
+    'percent_unreviewed_auto_translations' => 96,
+    'num_unreviewed_auto_translations' => '1144',
+    'translators' => 
+    array (
+    ),
+  ),
+  123 => 
+  array (
+    'language_name' => 'Turkish',
+    'php_export_status' => 'Complete',
+    'php_filename' => 'tr.php',
+    'percent_reliability' => '0',
+    'percent_translated' => '100',
+    'percent_unreviewed_auto_translations' => 81,
+    'num_unreviewed_auto_translations' => '968',
+    'translators' => 
+    array (
+    ),
+  ),
+  127 => 
+  array (
+    'language_name' => 'Ukrainian',
+    'php_export_status' => 'Complete',
+    'php_filename' => 'uk.php',
+    'percent_reliability' => '0',
+    'percent_translated' => '100',
+    'percent_unreviewed_auto_translations' => 100,
+    'num_unreviewed_auto_translations' => '1189',
+    'translators' => 
+    array (
+    ),
+  ),
+  130 => 
+  array (
+    'language_name' => 'Vietnamese',
+    'php_export_status' => 'Complete',
+    'php_filename' => 'vi.php',
+    'percent_reliability' => '0',
+    'percent_translated' => '100',
+    'percent_unreviewed_auto_translations' => 100,
+    'num_unreviewed_auto_translations' => '1189',
+    'translators' => 
+    array (
+    ),
+  ),
+  20 => 
+  array (
+    'language_name' => 'Welsh',
+    'php_export_status' => 'Complete',
+    'php_filename' => 'cy.php',
+    'percent_reliability' => '0',
+    'percent_translated' => '100',
+    'percent_unreviewed_auto_translations' => 100,
+    'num_unreviewed_auto_translations' => '1189',
+    'translators' => 
+    array (
+    ),
+  ),
+  54 => 
+  array (
+    'language_name' => 'Yiddish',
+    'php_export_status' => 'Complete',
+    'php_filename' => 'yi.php',
+    'percent_reliability' => '0',
+    'percent_translated' => '100',
+    'percent_unreviewed_auto_translations' => 100,
+    'num_unreviewed_auto_translations' => '1189',
+    'translators' => 
+    array (
+    ),
+  ),
+);
+
+?>
